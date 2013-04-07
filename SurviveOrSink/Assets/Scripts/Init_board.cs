e0adb5aaa2f4e18f7f1aa27cc3f6a0c150f55676
Zach <blink753@tamu.edu>
2013-04-07 15:54:07 -0500

    Display up
    Added a display to post messages but Have not figured out how to make it
    visible to other scripts

--- SurviveOrSink/Assets/Scripts/Init_board.cs
+++ SurviveOrSink/Assets/Scripts/Init_board.cs
@@ -8,19 +8,35 @@ public class Init_board : MonoBehaviour
 	public GUIStyle quit;
 	public static GameObject[,] gameGrid = new GameObject[10,10];
 	public static GameObject[,] observationGrid = new GameObject[10,10];
-	
+	public Vector2 scrollPosition = new Vector2(100,100);
+	static string messages = "Begin Game!\nHi\nHello\n\nHello\n\n\n\nHello\n\n\nHello\n\n\n\nHit at A3\n\n\nThe End";
 	
 	void OnGUI()
 	{
 		var threeQuartersWidth = (int)(3 * Screen.width/4);
-		if( (int) Time.timeSinceLevelLoad < 5)
+		if( (int) Time.timeSinceLevelLoad < 3)
 		{
 			GUI.Label(new Rect(threeQuartersWidth,25,100,100), "BEGIN GAME!");
 		}
 		
 		if(GUI.Button(new Rect(Screen.width-100,0,100,20),"",quit))
 			Application.Quit();
+		GUILayout.BeginArea(new Rect((Screen.width/2)+125,20,Screen.width/2-125,(Screen.height/2)-20));
+		//GUILayout.BeginArea(Rect((Screen.width/2)+125,20,Screen.width/2-125,(Screen.height/2)-20),"This is the text to be displayed");
+		//GUILayout.BeginArea(new Rect (100,100,100,100));
+		scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(Screen.width/2-125), GUILayout.Height((Screen.height/2)-20));
+		GUILayout.Label(messages);
+		if (GUILayout.Button("Clear"))
+            messages = "";
+		GUILayout.EndScrollView();
+		if (GUILayout.Button("Add More Text"))
+            messages += "\nHere is another line";
+		 GUILayout.EndArea ();
 		
+		//scrollPosition = GUI.BeginScrollView(new Rect((Screen.width/2)+125,20,Screen.width/2-125,(Screen.height/2)-20),scrollPosition,new Rect((Screen.width/2)+125,20,Screen.width/2-125,(Screen.height/2)-20));
+		//scrollPosition = GUI.BeginScrollView(new Rect((Screen.width/2)+125,20,(Screen.width/2)-20,(Screen.height/2)-20),scrollPosition,new Rect((Screen.width/2)+125,20,Screen.width/2-125,(Screen.height/2)-20),false,true);
+		//GUI.Label(new Rect((Screen.width/2)+125,20,Screen.width/2-125,(Screen.height/2)-20), messages);
+		//GUI.EndScrollView();
 	}
 	
 	// Use this for initialization
